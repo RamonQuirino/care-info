79a5e415276ff6b86cd36c6f0e9c8bec56653a67
Ramon Quirino <ramon.ti@hotmail.com>
2019-07-09 22:05:03 +0100

    adding start script and change port to 4000

--- care-info-backend/app.js
+++ care-info-backend/app.js
@@ -22,6 +22,6 @@ app.use("/graphql", ExpressGraphQL({
     rootValue: global,
 }));
 
-app.listen(3000, () => {
-    console.log("Listening at :3000...");
+app.listen(4000, () => {
+    console.log("Listening at :4000...");
 });
